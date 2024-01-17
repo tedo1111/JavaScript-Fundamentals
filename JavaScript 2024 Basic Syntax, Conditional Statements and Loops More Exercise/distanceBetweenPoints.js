ad108e87ba94afe725c582e8e7fd53b2c291956a
edo1111` <edo1998@abv.bg`>
2024-01-17 17:47:28 +0200

    Ex. 3 - 5

---
+++ JavaScript 2024 Basic Syntax, Conditional Statements and Loops More Exercise/distanceBetweenPoints.js
@@ -0,0 +1,8 @@
+function distanceBetweenPoints(x1, y1, x2, y2) {
+
+    let res = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
+    console.log(res);
+
+}
+distanceBetweenPoints(2, 4, 5, 0);
+distanceBetweenPoints(2.34, 15.66, -13.55, -2.9985);
--- JavaScript 2024 Basic Syntax, Conditional Statements and Loops More Exercise/reverseString.js
+++ JavaScript 2024 Basic Syntax, Conditional Statements and Loops More Exercise/reverseString.js
@@ -1,8 +1,13 @@
-function reverseString() {
+function reverseString(reverseWord) {
 
+    let word = "";
 
+    for (let i = reverseWord.length - 1; i >= 0; i--) {
+        word += (reverseWord[i]);
 
+    }
+    console.log(`${word}`);
 }
-reverseString(Hello);
-reverseString(SoftUni);
-reverseString(1234);
+reverseString(`Hello`);
+reverseString(`SoftUni`);
+reverseString(`1234`);

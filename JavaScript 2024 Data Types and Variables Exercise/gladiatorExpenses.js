b87d93140f3521fb177ee115b8ca68930937067d
edo1111` <edo1998@abv.bg`>
2024-01-22 02:12:51 +0200

    Ex. 9

--- JavaScript 2024 Data Types and Variables Exercise/gladiatorExpenses.js
+++ JavaScript 2024 Data Types and Variables Exercise/gladiatorExpenses.js
@@ -1,7 +1,30 @@
-function gladiatorExpenses() {
+function gladiatorExpenses(lostFights, helmetPrice, swordPirce, shieldPrice, armorPrice) {
 
+    let helmet = 0;
+    let sword = 0;
+    let shield = 0;
+    let armor = 0;
 
+    for (let i = 1; i <= lostFights; i++) {
+        if (i % 2 === 0) {
+            helmet++;
+        }
 
+        if (i % 3 === 0) {
+            sword++;
+        }
+
+        if (i % 2 === 0 && i % 3 === 0) {
+            shield++;
+
+        }
+    }
+
+    armor = Math.floor(shield / 2);
+
+    let total = helmet * helmetPrice + sword * swordPirce + shield * shieldPrice + armor * armorPrice;
+
+    console.log(`Gladiator expenses: ${(total).toFixed(2)} aureus`)
 }
 gladiatorExpenses(7, 2, 3, 4, 5);
 gladiatorExpenses(23, 12.50, 21.50, 40, 200);

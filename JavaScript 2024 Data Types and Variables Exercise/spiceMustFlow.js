b449ea5073a5967d76735d6d29af48fab39c6372
edo1111` <edo1998@abv.bg`>
2024-01-22 19:07:42 +0200

    Ex. 10

--- JavaScript 2024 Data Types and Variables Exercise/spiceMustFlow.js
+++ JavaScript 2024 Data Types and Variables Exercise/spiceMustFlow.js
@@ -1,7 +1,36 @@
-function spiceMustFlow() {
+function spiceMustFlow(starting) {
+
+    let days = 0;
+
+    let spices = starting;
+    let forWorkers = 26;
+
+    let totalSpices = 0;
+    let totalYield = starting;
+
+    let forPeople = 0;
+
+    let finalResult = 0;
 
 
+    forPeople = totalYield - 26;
+    while (spices >= 100) {
 
+        totalSpices = spices - forWorkers;
+        finalResult += totalSpices;
+        totalYield = spices - 10;
+        spices = totalYield;
+
+        days++;
+    }
+    let result = 0;
+
+    if (totalSpices > 26) {
+        result = finalResult - 26;
+    }
+
+    console.log(days);
+    console.log(result);
 }
-spiceMustFlow(111);
+// spiceMustFlow(111);
 spiceMustFlow(450);

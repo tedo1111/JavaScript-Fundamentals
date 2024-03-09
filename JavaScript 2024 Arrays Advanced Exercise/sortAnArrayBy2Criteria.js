884206fe2a8bd2f6e21dfcac4dde60d5e236ab05
edo1111` <edo1998@abv.bg`>
2024-03-09 16:32:13 +0200

    no no no

--- JavaScript 2024 Arrays Advanced Exercise/sortAnArrayBy2Criteria.js
+++
@@ -1,8 +0,0 @@
-function sortAnArrayBy2Criteria(arr) {
-
-    let sorted = arr.sort((a, b) => a.length - b.length || a.localeCompare(b))
-
-    console.log(sorted.join(`\n`));
-}
-sortAnArrayBy2Criteria(['alpha', 'beta', 'gamma']);
-sortAnArrayBy2Criteria(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);

304f728794c0fbce1ecd85eddd23906637f769e8
edo1111` <edo1998@abv.bg`>
2024-03-31 00:15:20 +0200

    Fix

--- JavaScript 2024 Associative Arrays Exercise/travelTime.js
+++ JavaScript 2024 Associative Arrays Exercise/travelTime.js
@@ -1,5 +1,54 @@
-function travelTime() {
+function travelTime(input) {
 
+    let destinations = {};
+
+    for (let el of input) {
+
+        let [countryName, townName, travelcost] = el.split(" > ");
+        travelcost = Number(travelcost);
+
+
+        if (!destinations.hasOwnProperty(countryName)) {
+            destinations[countryName] = {};
+        }
+
+        if (!destinations[countryName].hasOwnProperty(townName)) {
+            destinations[countryName][townName] = travelcost;
+        }
+        else {
+            destinations[countryName][townName] = Math.min(destinations[countryName][townName], travelcost);
+        }
+    }
+
+    let sortedCountries = Object.entries(destinations)
+        .sort((a, b) => a[0].localeCompare(b[0]))
+        .reduce((acc, [country, towns]) => {
+            let sortedTowns = Object.entries(towns)
+                .sort((a, b) => a[1] - b[1])
+                .map(([town, cost]) => `${town} -> ${cost}`);
+
+            acc.push(`${country} -> ${sortedTowns.join(' ')}`);
+            return acc;
+        }, []);
+
+    console.log(sortedCountries.join('\n'));
 }
-travelTime();
-travelTime();
+travelTime(
+    [
+        "Bulgaria > Sofia > 500",
+        "Bulgaria > Sopot > 800",
+        "France > Paris > 2000",
+        "Albania > Tirana > 1000",
+        "Bulgaria > Sofia > 200"
+    ]
+
+);
+// travelTime([
+//     'Bulgaria > Sofia > 25000',
+//     'Bulgaria > Sofia > 25000',
+//     'Kalimdor > Orgrimar > 25000',
+//     'Albania > Tirana > 25000',
+//     'Bulgaria > Varna > 25010',
+//     'Bulgaria > Lukovit > 10'
+// ]
+// );
--- testFile.js
+++ testFile.js
@@ -1,41 +1,8 @@
-function testFile(products) {
+function testFile(input) {
 
-    let listProducts = [];
-
-    for (let i = 0; i < products.length; i++) {
-        let [name, price] = products[i].split(" : ");
-        listProducts.push({ name, price: Number(price) });
-    }
-
-    listProducts.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
-
-    let groupedProduts = {};
-
-
-    for (let i = 0; i < listProducts.length; i++) {
-        let initial = listProducts[i].name[0].toUpperCase();
-        if (!groupedProduts[initial]) {
-            groupedProduts[initial] = [];
-        }
-        groupedProduts[initial].push(listProducts[i]);
-    }
-
-    Object.keys(groupedProduts).forEach(initial => {
-        console.log(initial);
-        groupedProduts[initial].forEach(products => {
-            console.log(`  ${products.name}: ${products.price}`);
-        })
-    })
 
 }
-testFile([
-    'Appricot : 20.4',
-    'Fridge : 1500',
-    'TV : 1499',
-    'Deodorant : 10',
-    'Boiler : 300',
-    'Apple : 1.25',
-    'Anti-Bug Spray : 15',
-    'T-Shirt : 10'
-]
+testFile(
+
 );
+
